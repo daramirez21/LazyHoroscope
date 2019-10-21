e640b2915750cb8d4c2067b96f573b44f825810c
daramirez21 <daramirez@mail.csuchico.edu>
2019-10-21 15:54:33 -0700

    Fixes it

--- main.cpp
+++ main.cpp
@@ -1,4 +1,4 @@
-//Authors:
+//Authors: David Ramirez
 #include<iostream>
 #include<string>
 
@@ -7,10 +7,50 @@ using namespace std;
 int main()
 {
   //declare variables
+  string first_name;
+  string last_name;
+
 
   //get user input
+  cout << "What is your first name?" << endl;
+  cin >> first_name;
+ 
+  cout << "What is your last name?" << endl;
+  cin >> last_name;
 
+  cout << "Welcome, " <<first_name.at(0) << "." <<last_name.at(0)<< "., here is your fortune...\n";
   //tell fortune
+  cout << "your lucky number is " <<first_name.length()<< endl;
+  
+
+ int index;
+
+ index = last_name.length()-1;
+
+
+  first_name.at(0) = toupper(first_name.at(0)); 
+
+  last_name.at(index) = toupper(last_name.at(index));
+
+  if(first_name.at(0) == 'A' || first_name.at(0) == 'E' || first_name.at(0) == 'I' || first_name.at(0) == 'O' || first_name.at(0) == 'U')
+ {
+ cout << "you are destined to be famous!\n";
+ }
+
+ else
+ {
+ cout << "you should keep a low profile.\n";
+ }
+ 
+
+
+
+ if(last_name.at(index) == 'A' || last_name.at(index) == 'E' || last_name.at(index) == 'I' || last_name.at(index) == 'O' || last_name.at(index) == 'U' )
+ {
+ cout << "you have already met your true love.\n";
+ }
+ 
+ cout << "have a good day!\n";
 
   return 0;
 }
